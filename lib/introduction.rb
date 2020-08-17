7a578d5a3f14d0486a03128bb321c422c0935531
Rodney Smith II <r_smith3@hotmail.com>
2020-08-17 22:25:50 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -13,7 +13,7 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learnng to program in #{language}."
 end
 
-introduction_with_language("Dan", "Ember")
+introduction_with_language("Dan", "Ember.js")
 
 
 
